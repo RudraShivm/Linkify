14248a8a0e5ab9c1e746ba59747bf088b5944801
Dibbo Chowdhury <66284130+RudraShivm@users.noreply.github.com>
2024-06-07 01:37:38 +0600

    Update server.js

--- server.js
+++ server.js
@@ -88,23 +88,7 @@ const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 // const upload2 = multer({ dest: "uploads/" });
 app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://vermillion-marzipan-f7ee38.netlify.app"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Private-Network", true);
-  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
-  res.setHeader("Access-Control-Max-Age", 7200);
-
+  res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 const stripeInstance = stripe(
