9c607273f1119ddf5039021eda1dad7103e0b59a
Dibbo Chowdhury <66284130+RudraShivm@users.noreply.github.com>
2024-06-06 20:57:07 +0600

    Update server.js

--- server.js
+++ server.js
@@ -82,13 +82,31 @@ import {
 } from "./src/backend/database.js";
 const app = express();
 // app.use(cors());
-app.use(cors({ origin: "https://vermillion-marzipan-f7ee38.netlify.app" }));
 // app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 // const upload2 = multer({ dest: "uploads/" });
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://vermillion-marzipan-f7ee38.netlify.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  //  Firefox caps this at 24 hours (86400 seconds). Chromium (starting in v76) caps at 2 hours (7200 seconds). The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
 
+  next();
+});
 const stripeInstance = stripe(
   "sk_test_51OiJl9Lgd2pARuX3fCxFbkgZuJGLzlTJi4dzbjJss22wyOlj9UeD64HTy0XlTYrtdH2Y3PUPoG8QiT9TEQKOsV3I00r6vLRxn0"
 );
